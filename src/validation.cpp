0bc092618b4350842e9adf0fab1590beb4d1f5ff
Harris Brakmic <brakmic@gmail.com>
2018-04-29 16:36:20 +0200

    use original validation rules

--- src/validation.cpp
+++ src/validation.cpp
@@ -1069,7 +1069,7 @@ bool IsInitialBlockDownload()
         return true;
     if (chainActive.Tip()->nChainWork < nMinimumChainWork)
         return true;
-    if (chainActive.Tip()->nChainWork < UintToArith256(chainParams.GetConsensus().nMinimumChainWork))
+    if (chainActive.Tip()->GetBlockTime() < (GetTime() - nMaxTipAge))
         return true;
     LogPrintf("Leaving InitialBlockDownload (latching to false)\n");
     latchToFalse.store(true, std::memory_order_relaxed);
